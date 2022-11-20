69177d2edb1734903422902431d6d6a4d348ac4f
Explorer <shatl030303@gmail.com>
2022-11-20 23:15:02 +0300

    initial

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'calc/input'
+  get 'calc/view'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: redirect('/calc/input')
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
